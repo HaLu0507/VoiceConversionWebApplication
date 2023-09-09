328a65f54f810628fddb756791e1e2366d8cc410
sabatexima <kinokosaibaiki@i.softbank.jp>
2023-09-09 17:27:07 +0900

    [Add] index

--- sample/flask/app.py
+++ sample/flask/app.py
@@ -2,16 +2,19 @@ from flask import Flask, render_template, request, send_from_directory
 import os 
 
 app = Flask(__name__)
-
+file_name = ""
+file_path = ""
 #最初の画面の表示
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('sample.html')
 
 #音声を再生するためのメソッド？
-@app.route("/music/sampleAudio.m4a")
+@app.route('/music')
 def play_music():
-    return send_from_directory("music", "sampleAudio.m4a")
+    global file_name
+    return send_from_directory("music", file_name)
 
 #音声ファイルを取得するメソッド
 @app.route('/second', methods=['GET', 'POST'])
@@ -19,6 +22,8 @@ def upload_file():
     #htmlでアップロードされたファイルを取得
     file = request.files['file']
     #保存先のパスとファイル名を指定
+    global file_name
+    file_name = str(file.filename)
     file_path = os.path.join('./music/', file.filename)
     #指定した形式で保存
     file.save(file_path)
