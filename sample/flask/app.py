af98b45b7050f0a4fc28e1d200b57fd6c4dcc738
sabatexima <kinokosaibaiki@i.softbank.jp>
2023-09-09 18:14:40 +0900

    [Add] index

--- sample/flask/app.py
+++ sample/flask/app.py
@@ -4,7 +4,8 @@ import os
 app = Flask(__name__)
 
 #ファイルの名前
-file_name = ""
+file_name_before = ""
+file_name_after = ""
 
 
 #最初の画面の表示
@@ -13,13 +14,22 @@ def index():
     return render_template('sample.html')
 
 #音声を再生するためのメソッド？
-@app.route('/music')
-def play_music():
-    global file_name
-    
+@app.route('/beforeMusic')
+def play_music_before():
+    global file_name_before
+
     #第一引数が取得したいファイルのディレクトリ名、
     #第二引数が取得したいファイルのファイル名
-    return send_from_directory("music", file_name)
+    return send_from_directory("music", file_name_before)
+
+#音声を再生するためのメソッド？
+@app.route('/afterMusic')
+def play_music_after():
+    global file_name_after
+
+    #第一引数が取得したいファイルのディレクトリ名、
+    #第二引数が取得したいファイルのファイル名
+    return send_from_directory("music", file_name_after)
 
 #音声ファイルを取得するメソッド
 @app.route('/second', methods=['GET', 'POST'])
@@ -28,16 +38,20 @@ def upload_file():
     file = request.files['file']
 
     #保存先のファイル名を指定
-    global file_name
-    file_name = str(file.filename)
+    global file_name_before
+    file_name_before = str(file.filename)
 
-    if file_name == "":
+    if file_name_before == "":
         return render_template('sample.html')
     #保存先のパスとファイル名を指定
     file_path = os.path.join('./music/', file.filename)
 
     #指定した形式で保存
     file.save(file_path)
+
+    #変換後の音声
+    global file_name_after
+    file_name_after = str(file.filename)
 
     #次の外面に遷移する
     #nameはファイルのパス
