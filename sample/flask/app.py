ff252a16a66b8a3d692c9638de428c7745c68510
sabatexima <kinokosaibaiki@i.softbank.jp>
2023-09-09 15:51:39 +0900

    [Add] index

--- sample/flask/app.py
+++ sample/flask/app.py
@@ -1,26 +1,18 @@
 from flask import Flask, render_template, request
-from sentimentAnalyze import getSentiment
-from getSimilarity import calcSimilarity
+import os 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST': # データを受け取るとき
-        text = request.form.get('user_tweet')
-        usr_feel, usr_feel_point = getSentiment(text)
-        usr_feeling = f"{usr_feel}(score : {usr_feel_point})"
+    return render_template('sample.html')
 
-        if usr_feel == "ポジティブ":
-            artist, song, score = calcSimilarity(text, True)
-        else:
-            artist = "Nothing"
-            song = "Nothing"
-            score = "None"
-
-        return render_template('index.html', text=text, usr_feeling=usr_feeling, artist_name=artist, song_name=song, song_feeling="ポジティブ", similar_point=score)
-    else: # データを渡すとき
-        return render_template('index.html')
+@app.route('/second', methods=['GET', 'POST'])
+def upload_file():
+    file = request.files['file']
+    file_path = os.path.join('./', file.filename)
+    file.save(file_path)
+    return render_template('post.html', name = file_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
