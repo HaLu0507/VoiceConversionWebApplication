358e7e9f5757de8d8d6cc78a39113bd0b5f872d4
sabatexima <kinokosaibaiki@i.softbank.jp>
2023-09-09 16:49:55 +0900

    [Add] index

--- sample/flask/app.py
+++ sample/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import os 
 
 app = Flask(__name__)
@@ -7,12 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('sample.html')
 
+@app.route("/music/sampleAudio.m4a")
+def play_music():
+    return send_from_directory("music", "sampleAudio.m4a")
+
 @app.route('/second', methods=['GET', 'POST'])
 def upload_file():
     file = request.files['file']
-    file_path = os.path.join('./', file.filename)
+    file_path = os.path.join('./music/', file.filename)
     file.save(file_path)
-    return render_template('post.html',name = (file.filename))
+    return render_template('post.html',name = file_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
