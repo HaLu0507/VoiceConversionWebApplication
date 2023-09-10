769344bbe7279a01284482def9a16e15b55e72dd
Halu <tanaka0507official@gmail.com>
2023-09-10 13:53:16 +0900

    enhance audio quality

--- sample/flask/models/converters/CycleGAN_VC2/convert.py
+++ sample/flask/models/converters/CycleGAN_VC2/convert.py
@@ -6,10 +6,44 @@ import numpy as np
 import os 
 import soundfile as sf
 
-def convert(file_name, file_path):
-    """ 引数で与えられた音声ファイルを変換し, 変換した音声ファイルを保存するメソッド
+# melGAN
+vocoder = torch.hub.load('descriptinc/melgan-neurips', 'load_melgan', model_name='multi_speaker')
+
+def normalize_mel(wav):
+    """ 音声をメルスペクトログラムに変換し, 正規化するメソッド
 
     Args:
+        wav numpy(Time,) : 音声データ
+    
+    Return:
+        mel_normalized torch(1, Hz, Time) : 正規化されたメルスペクトログラム
+    """
+
+    # wav -> mel-spec
+    spec = vocoder(torch.tensor([wav]))
+    print(f"spec shape : {spec.shape}") # maybe (1, Hz, Time)
+    # tensor -> numpy
+    spec = spec.cpu().detach().numpy()[0] # spec shape : (Hz, Time)
+
+    # get mean & std
+    mel_mean = np.mean(spec, axis=1, keepdims=True)
+    mel_std = np.std(spec, axis=1, keepdims=True) + 1e-9
+    # normalize mel-spec
+    mel_normalized = (spec - mel_mean) / mel_std
+
+    # numpy -> tensor
+    mel_normalized = torch.from_numpy(mel_normalized).float()
+    # shape(Hz, Time) -> shape(1, Hz, Time)
+    mel_normalized = mel_normalized.unsqueeze(0)
+
+    return mel_normalized
+    
+def convert(file_name, file_path):
+    """ 引数で与えられた音声ファイルを変換し, 変換した音声ファイルを保存するメソッド
+        convert()メソッドと異なり正規化を行なっている.
+
+    Args:
+        file_name : 音声のファイル名
         file_path : 変換前の音声のパス
     """
 
@@ -20,7 +54,6 @@ def convert(file_name, file_path):
     # モデルの生成
     generatorF2M = Generator().to('cpu')
     generatorM2F = Generator().to('cpu')
-    hub_vocoder = torch.hub.load('descriptinc/melgan-neurips', 'load_melgan', model_name='multi_speaker')
 
     # モデルのチェックポイントをロードする
     checkPoint = torch.load("./models/converters/CycleGAN_VC2/_CycleGAN_CheckPoint", torch.device('cpu'))
@@ -29,49 +62,52 @@ def convert(file_name, file_path):
     generatorM2F.load_state_dict(
         state_dict=checkPoint['model_genB2A_state_dict'])
     
+    # Speech Parameters
+    mel_sps_normalization = np.load("./models/converters/CycleGAN_VC2/norm_stats/mel_sps_normalization.npz")
+    female_mel_mean = mel_sps_normalization['mean_A']
+    female_mel_std = mel_sps_normalization['std_A']
+    male_mel_mean = mel_sps_normalization['mean_B']
+    male_mel_std = mel_sps_normalization['std_B']
+    
     # 前処理
     # hyper parameters
     n_mel_channels = 80
     sampling_rate = 22050 # default : 16000
     frame_period = 5.0
 
-    print(f"file path : {file_path}")
+    # 音声データの読み込み
     wav, _ = librosa.load(file_path, sr=sampling_rate, mono=True)
     wav = preprocess.wav_padding(wav=wav,
                                     sr=sampling_rate,
                                     frame_period=frame_period,
                                     multiple=4)
     
-    # tensor に変換し, 型を(1,Time)に変換する
-    wav = torch.from_numpy(wav).float()
-    wav = wav.unsqueeze(0)
-    # numpy に型を戻す
-    wav = wav.to('cpu').detach().numpy().copy()
-    
-    # getting data B mel spectrogram
-    mel_sp = preprocess.melGAN_decompose(wav, n_mel_channels) # 入力が(1,Time)の音声しか無理な点に注意
-    mel_sp_transposed = mel_sp.T
-    mel_sp_norm = np.array([mel_sp_transposed])
-    mel_sp_norm = torch.from_numpy(mel_sp_norm).float()
+    # 正規化されたメルスペクトログラムを取得
+    mel_normalized = normalize_mel(wav)
 
-    # convert , output: (batch size, Hz, time)
-    mel_sp_converted_norm = generatorM2F(mel_sp_norm)
+    # conversion
+    # converted_mel_normalized : torch(1, Hz, Time)
+    converted_mel_normalized = generatorM2F(mel_normalized)
+
+    # torch -> numpy & mel_normalized -> mel & numpy -> torch
+    converted_mel_normalized = converted_mel_normalized.to('cpu').detach().numpy().copy()
+    converted_mel = (converted_mel_normalized * female_mel_std) + female_mel_mean
+    converted_mel = torch.from_numpy(converted_mel.astype(np.float32)).clone()
 
     # mel sp -> wav
     # input  : tensor(batch size, Hz=80, time)
     # output : tensor(batch size, time)
-    wav_transformed = hub_vocoder.inverse(mel_sp_converted_norm) 
+    wav_transformed = vocoder.inverse(converted_mel) 
     wav_transformed = wav_transformed.squeeze(0) # wav_transformed (time)
     wav_transformed = wav_transformed.to('cpu').detach().numpy().copy() # tensor to numpy
     
     # librosa の output 関数はなかったので, wavファイルへの変換は soundfile モジュールを使用した
     sf.write(f"{out_dir}{file_name}",
-                                wav_transformed,
-                                sampling_rate,
-                                subtype="PCM_24")
-    
-    if __name__ == '__main__':
-        file_name = "sampleAudio.wav"
-        file_path = "/Users/Tanaka/VoiceConversionWebApplication/sample/flask/music/sampleAudio.wav"
-        print("process")
-        convert(file_name, file_path)
+             wav_transformed,
+             sampling_rate,
+             subtype="PCM_24")
+
+if __name__ == '__main__':
+    file_name = "sampleAudio.wav"
+    file_path = "/Users/Tanaka/VoiceConversionWebApplication/sample/flask/music/sampleAudio.wav"
+    convert(file_name, file_path)
