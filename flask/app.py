2037b26f31abcff61b2ae8b599dfa971da12315c
sabatexima <kinokosaibaiki@i.softbank.jp>
2023-12-23 01:23:09 +0900

    モード選択の追加

--- flask/app.py
+++ flask/app.py
@@ -1,97 +1,111 @@
-from flask import Flask, render_template, request, send_from_directory
+# coding: UTF-8
+from flask import Flask, render_template, request, send_from_directory, jsonify
 import os 
-from models.converters.CycleGAN_VC2.convert import convert
+from selectModel import selectModel
+from models.preprocess.removalNoise import removalBackgroundNoise
+from makeSps import saveSps
 
 app = Flask(__name__)
 
-#変換前のファイルの名前
-file_name_before = ""
-#変換後のファイルの名前
-file_name_after = ""
+count = 0
 
-def convertExt(file_name):
-    """ ユーザから得られた音声データの拡張子をwavに変更するメソッド
+def convertWav(file_name):
+    """ ユーザから得られた音声データをwav形式に変換するメソッド
 
     Args:
-        file_name : 音声データのファイル名
+        file_name : 拡張子付きの音声データのファイル名
 
     Returns: 拡張子をwavにしたファイル名
     """
+    os.system(f"ffmpeg -y -i ./audio/origin/{file_name} ./audio/origin/{file_name.split('.')[0]}.wav")
 
-    # 拡張子を取り出す
-    ext = file_name.split('.')[-1]
-    print(f"拡張子 : {ext}")
-    # ファイル名を取り出す
-    name = file_name.split('.')[0]
-    print(f"ファイル名 : {name}")
-    # 音声ファイルがあるディレクトリ
-    path = "./music"
-    print(f"パス : {path}")
+#音声をhtmlに送信するメソッド
+@app.route("/audio/<path:filename>")
+def play_before(filename):
+    #第一引数が取得したいファイルのディレクトリ名、
+    #第二引数が取得したいファイルのファイル名
+    return send_from_directory("audio", filename)
 
-    if ext != "wav": # wav 以外
-        os.system(f"afconvert -f WAVE -d LEI24 {path}/{name}.{ext} {path}/{name}.wav")
 
-    return f"{name}.wav"
+#画像をhtmlに送信するメソッド
+@app.route("/sps/<path:filename>")
+def show_mel_converted(filename):
+    #第一引数が取得したいファイルのディレクトリ名、
+    #第二引数が取得したいファイルのファイル名
+    return send_from_directory("sps", filename)
 
 
 #最初の画面の表示
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('sample.html')
-
-#音声を再生するためのメソッド？
-@app.route('/beforeMusic')
-def play_music_before():
-    global file_name_before
-
-    #第一引数が取得したいファイルのディレクトリ名、
-    #第二引数が取得したいファイルのファイル名
-    return send_from_directory("music", file_name_before)
-
-#音声を再生するためのメソッド？
-@app.route('/afterMusic')
-def play_music_after():
-    global file_name_after
-
-    #第一引数が取得したいファイルのディレクトリ名、
-    #第二引数が取得したいファイルのファイル名
-    return send_from_directory("music", file_name_after)
+    global count
+    count = 0
+    return render_template('login.html')
 
 
 #音声ファイルを取得するメソッド
-@app.route('/second', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    
+    # フォームから送信されたユーザー名とパスワードを取得
+    global count
+    if(count == 0):
+        entered_password = request.form['data1']
+        if(not entered_password == "webApp2023"):
+            return render_template('login.html')
+        else:
+            count += 1
+            return render_template('post.html', boolean=False)
+        
+    #変換が男性から女性か女性から男性かの文字列
+    #男性から女性:"convertM2W",  女性から男性:"convertW2M"
+    mode = request.form.get('sel')
+
+    #変換の手法の文字列
+    #CycleGAN_VC2、MaskCycleGAN_VC
+    method = request.form.get('method')
+
+    print(method)
     #htmlでアップロードされたファイルを取得
     file = request.files['file']
+    print(request.files['file'])
 
-    #保存先のファイル名を指定
-    global file_name_before
-    file_name_before = str(file.filename)
 
-    if file_name_before == "":
-        return render_template('sample.html')
+    #保存先のファイル名を指定(拡張子なし)
+    origin_name = str(file.filename).split('.')[0]
+
+    #ファイルを選択しなかったときの処理
+    if origin_name == "":
+        return render_template('post.html',error = "ファイルを選択してください",boolean = False)
     
-    #保存先のパスとファイル名を指定
-    file_path = os.path.join('./music/', file.filename)
+    #modeを何も選択しなかったときの処理
+    if mode == None:
+        return render_template('post.html', error="変換方法を選択してください",boolean = False)
+    
+
+    #保存先の絶対パスとファイル名を指定
+    origin_path = os.path.join(os.getcwd(), 'audio/origin/', file.filename)
     #指定した形式で保存
-    file.save(file_path)
+    file.save(origin_path)
 
-    # 拡張子の変更
-    file_name_before = convertExt(file_name_before)
+    if file.filename.split('.')[-1] != "wav": # 拡張子がwavでない場合は変換する
+        convertWav(file.filename)
+
     # 参照するパスとファイル名を変更
-    file_path = os.path.join('./music/', file_name_before)
+    origin_path = os.path.join(os.getcwd(), f"audio/origin/{origin_name}.wav")
 
-    #変換後の音声
-    global file_name_after
-    file_name_after = str(file.filename).split(".")[0]
-    file_name_after = "converted_" + str(file_name_after) +".wav"
+    # ユーザがアップロードした音声の背景ノイズを除去する
+    removalBackgroundNoise(origin_path)
 
-    convert(file_name=file_name_before, file_path=file_path)
+    # 音声変換
+    selectModel(modelName=method, origin_path=origin_path, mode=mode)
+
+    # スペクトログラムの画像を求める
+    saveSps(origin_name)
 
     #次の外面に遷移する
-    #nameはファイルのパス
-    return render_template('post.html')
+    #fileBは変換前の音声ファイル、fileAは変換後の音声ファイル
+    return render_template('post.html', file_name=str(origin_name), boolean=True)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=5010)
--- flask/models/converters/CycleGAN_VC2/convert.py
+++ flask/models/converters/CycleGAN_VC2/convert.py
@@ -1,10 +1,16 @@
-from .Generator import Generator
+# このファイルを直接実行する場合
+from Generator import Generator
+import preprocess
+
+# このファイルを直接実行しない場合
+# from .Generator import Generator
+# from . import preprocess
+
 import torch
 import librosa 
-from . import preprocess
 import numpy as np
-import os 
 import soundfile as sf
+import argparse
 
 # melGAN
 vocoder = torch.hub.load('descriptinc/melgan-neurips', 'load_melgan', model_name='multi_speaker')
@@ -21,7 +27,7 @@ def normalize_mel(wav):
 
     # wav -> mel-spec
     spec = vocoder(torch.tensor([wav]))
-    print(f"spec shape : {spec.shape}") # maybe (1, Hz, Time)
+    # print(f"spec shape : {spec.shape}") # maybe (1, Hz, Time)
     # tensor -> numpy
     spec = spec.cpu().detach().numpy()[0] # spec shape : (Hz, Time)
 
@@ -38,32 +44,42 @@ def normalize_mel(wav):
 
     return mel_normalized
     
-def convert(file_name, file_path):
+def convert(origin_path, mode):
     """ 引数で与えられた音声ファイルを変換し, 変換した音声ファイルを保存するメソッド
         convert()メソッドと異なり正規化を行なっている.
 
     Args:
-        file_name : 音声のファイル名
-        file_path : 変換前の音声のパス
+        origin_path : 変換前の音声のパス(拡張子付き)
+        mode : convertM2W if male -> female else convertW2M
     """
 
     # 変換した音声を保存するディレクトリ
     # app.py から見た時の相対パスになっている
-    out_dir = "./music/converted_" 
+    # out_dir = "./music/converted_" 
+    # 絶対パス
+    out_dir = f"{origin_path.split('/origin/')[0]}/converted/{origin_path.split('/')[-1]}"
+    print(f"our dir : {out_dir}")
 
     # モデルの生成
     generatorF2M = Generator().to('cpu')
     generatorM2F = Generator().to('cpu')
 
     # モデルのチェックポイントをロードする
-    checkPoint = torch.load("./models/converters/CycleGAN_VC2/_CycleGAN_CheckPoint", torch.device('cpu'))
+    # 直接 convert.py を用いないとき
+    # checkPoint = torch.load("./models/converters/CycleGAN_VC2/_CycleGAN_CheckPoint", torch.device('cpu'))
+    # 直接 convert.py を用いるとき
+    checkPoint = torch.load("./_CycleGAN_CheckPoint", torch.device('cpu'))
+
     generatorF2M.load_state_dict(
         state_dict=checkPoint['model_genA2B_state_dict'])
     generatorM2F.load_state_dict(
         state_dict=checkPoint['model_genB2A_state_dict'])
     
     # Speech Parameters
-    mel_sps_normalization = np.load("./models/converters/CycleGAN_VC2/norm_stats/mel_sps_normalization.npz")
+    # 直接 convert.py を用いないとき
+    # mel_sps_normalization = np.load("./models/converters/CycleGAN_VC2/norm_stats/mel_sps_normalization.npz")
+    # 直接 convert.py を用いるとき
+    mel_sps_normalization = np.load("./norm_stats/mel_sps_normalization.npz")
     female_mel_mean = mel_sps_normalization['mean_A']
     female_mel_std = mel_sps_normalization['std_A']
     male_mel_mean = mel_sps_normalization['mean_B']
@@ -76,7 +92,7 @@ def convert(file_name, file_path):
     frame_period = 5.0
 
     # 音声データの読み込み
-    wav, _ = librosa.load(file_path, sr=sampling_rate, mono=True)
+    wav, _ = librosa.load(origin_path, sr=sampling_rate, mono=True)
     wav = preprocess.wav_padding(wav=wav,
                                     sr=sampling_rate,
                                     frame_period=frame_period,
@@ -87,11 +103,18 @@ def convert(file_name, file_path):
 
     # conversion
     # converted_mel_normalized : torch(1, Hz, Time)
-    converted_mel_normalized = generatorM2F(mel_normalized)
+    if mode == "convertM2F": # convert male 2 female
+        converted_mel_normalized = generatorM2F(mel_normalized)
+    else: # convert female 2 male
+        converted_mel_normalized = generatorF2M(mel_normalized)
+
 
     # torch -> numpy & mel_normalized -> mel & numpy -> torch
     converted_mel_normalized = converted_mel_normalized.to('cpu').detach().numpy().copy()
-    converted_mel = (converted_mel_normalized * female_mel_std) + female_mel_mean
+    if mode == "convertM2F": # male 2 female
+        converted_mel = (converted_mel_normalized * female_mel_std) + female_mel_mean
+    else: # female 2 male
+        converted_mel = (converted_mel_normalized * male_mel_std) + male_mel_mean
     converted_mel = torch.from_numpy(converted_mel.astype(np.float32)).clone()
 
     # mel sp -> wav
@@ -102,12 +125,18 @@ def convert(file_name, file_path):
     wav_transformed = wav_transformed.to('cpu').detach().numpy().copy() # tensor to numpy
     
     # librosa の output 関数はなかったので, wavファイルへの変換は soundfile モジュールを使用した
-    sf.write(f"{out_dir}{file_name}",
+    sf.write(out_dir,
              wav_transformed,
              sampling_rate,
              subtype="PCM_24")
 
 if __name__ == '__main__':
-    file_name = "sampleAudio.wav"
-    file_path = "/Users/Tanaka/VoiceConversionWebApplication/sample/flask/music/sampleAudio.wav"
-    convert(file_name, file_path)
+
+    parser = argparse.ArgumentParser(description="Conversion settings")
+    
+    parser.add_argument('--origin_path', type=str, help="wav file path")
+    parser.add_argument('--mode', type=str, help="which conversion male to female or female to male")
+
+    argv = parser.parse_args()
+
+    convert(argv.origin_path, argv.mode)
