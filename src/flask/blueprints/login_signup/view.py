eca97d04667dec7c9ff4d94bb9d49949bc6a7b13
Halu <halu@handahisashinoMacBook-Pro.local>
2024-04-27 09:33:38 +0900

    ユーザの登録とログインのviewとmodelを追加

--- src/flask/blueprints/login_signup/view.py
+++ src/flask/blueprints/login_signup/view.py
@@ -1,13 +1,60 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+from .models.db_func import register, auth, isSameUser
 
-login_signup = Blueprint('login_signup', __name__, url_prefix='/blueprints/login_signup')
+login_signup = Blueprint('login_signup', __name__, template_folder='login_signup_templates')
 
-#サインアップ
-@login_signup.route('/signup', methods=['GET', 'POST'])
+# login
+@login_signup.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+# sign up 
+@login_signup.route('/sign_up', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    return render_template('sign_up.html')
 
-#サインアップ
-@login_signup.route('/modeSelect2', methods=['GET', 'POST'])
-def modeSelect2():
-    return render_template('modeSelect.html')
+# login, sign up 後の動作
+@login_signup.route('/modeSelect', methods=['GET', 'POST'])
+def modeSelect():
+    # login か sign up をURLを取得することにより判定する
+    login_or_signup = request.referrer.split('/')[-1]
+    print(f"送信元URL : {login_or_signup}")
+
+    # 入力されたユーザ名とパスワードを取得
+    entered_name = request.form['username']
+    entered_password = request.form['password']
+
+    if login_or_signup == "sign_up":
+        print(f"新たなユーザを登録する")
+        try:
+            # 同一ユーザがいるかどうか確認する
+            if isSameUser(entered_name):
+                print("同名のユーザが存在する")
+                return redirect("/sign_up")
+            
+            else: # 同一のユーザがいない場合、登録する
+                register(entered_name, entered_password)
+                return render_template("modeSelect.html")
+        
+        except Exception as e:
+            print(e)
+            return redirect('/sign_up')
+    
+    elif login_or_signup == "login":
+        print(f"ユーザの認証を行う")
+
+        try:
+            isAuth = auth(entered_name, entered_password)
+            if isAuth: # 認証が正しく行われた時
+                print("認証成功")
+                return render_template("modeSelect.html")
+            else: # 認証が失敗した時
+                print("認証失敗")
+                return redirect("/login")
+        
+        except Exception as e:
+            print(e)
+            return redirect("/login")
+    
+    else:
+        print("Error")
